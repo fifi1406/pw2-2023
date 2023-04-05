c018b54f1f8c2f860e8f09518e51a79a2d807747
fifi1406 <sitimasfiroh582@gmail.com>
2023-04-05 22:58:51 +0700

    Tugas&Praktik6

---
+++ pertemuan6/pelanggan/view_pelanggan.php
@@ -0,0 +1,52 @@
+<?php 
+require_once '../dbkoneksi.php';
+?>
+
+<?php
+    // Mendapatkan nilai id dari parameter GET
+    $_id = $_GET['id'];
+
+    // Membuat query SQL untuk mengambil data pelanggan dengan id tertentu
+    $sql = "SELECT * FROM pelanggan WHERE id=?";
+    $st = $dbh->prepare($sql);
+
+    // Menjalankan query dengan parameter id yang telah didapatkan sebelumnya
+    $st->execute([$_id]);
+
+    // Mengambil hasil query dan menyimpannya ke dalam variabel $row
+    $row = $st->fetch();
+?>
+
+<!-- Menampilkan data produk dalam bentuk tabel -->
+<table class="table table-striped">
+    <tbody>
+        <tr>
+            <td>Kode</td>
+            <td><?=$row['kode']?></td>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td><?=$row['nama']?></td>
+        </tr>
+        <tr>
+            <td>Jenis Kelamin</td>
+            <td><?=$row['jk']?></td>
+        </tr>
+        <tr>
+            <td>Tempat Lahir</td>
+            <td><?=$row['tmp_lahir']?></td>
+        </tr>
+        <tr>
+            <td>Tanggal Lahir</td>
+            <td><?=$row['tgl_lahir']?></td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td><?=$row['email']?></td>
+        </tr>
+        <tr>
+            <td>Kartu_id</td>
+            <td><?=$row['kartu_id']?></td>
+        </tr>
+    </tbody>
+</table>
